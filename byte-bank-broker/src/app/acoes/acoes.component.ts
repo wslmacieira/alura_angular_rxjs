0630c8f20fd2ed28a7007eba6027548b6302cac1
wslmacieira <wslmacieira@outlook.com>
2022-01-28 13:18:33 -0300

    SwitchMap e merge

--- byte-bank-broker/src/app/acoes/acoes.component.ts
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -1,9 +1,9 @@
-import { Subscription } from 'rxjs';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { merge } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
 
 import { AcoesService } from './acoes.service';
-import { Acoes } from './modelo/acoes';
 
 @Component({
   selector: 'app-acoes',
@@ -12,8 +12,15 @@ import { Acoes } from './modelo/acoes';
 })
 export class AcoesComponent {
   acoesInput = new FormControl();
-  acoes$ = this.acoesService.getAcoes();
+  todasAcoes$ = this.acoesService
+    .getAcoes()
+    .pipe(tap(() => console.log('Fluxo inicial')));
+  filtroPeloInput$ = this.acoesInput.valueChanges.pipe(
+    tap(() => console.log('Fluxo do filtro')),
+    switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado))
+  );
+
+  acoes$ = merge(this.todasAcoes$, this.filtroPeloInput$);
 
   constructor(private acoesService: AcoesService) {}
-
 }
--- byte-bank-broker/src/app/acoes/acoes.service.ts
+++ byte-bank-broker/src/app/acoes/acoes.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, pluck, tap } from 'rxjs/operators';
 
@@ -10,9 +10,10 @@ import { Acao, AcoesAPI } from './modelo/acoes';
 export class AcoesService {
   constructor(private httpCliente: HttpClient) {}
 
-  public getAcoes() {
+  public getAcoes(valor?: string) {
+    const params = valor ? new HttpParams().append('valor', valor) : undefined;
     return this.httpCliente
-      .get<AcoesAPI>('http://localhost:3000/acoes')
+      .get<AcoesAPI>('http://localhost:3000/acoes', { params })
       .pipe(
         tap((valor) => console.log(valor)),
         pluck('payload'),
